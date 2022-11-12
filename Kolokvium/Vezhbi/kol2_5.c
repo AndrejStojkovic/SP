afa8e51060df87d40319eaca8d0d0e2e5b24640e
Andrej Stojkovikj <40241148+AndrejStojkovic@users.noreply.github.com>
2022-11-12 14:46:03 +0100

    Update kol2_5.c

--- Kolokvium/Vezhbi/kol2_5.c
+++ Kolokvium/Vezhbi/kol2_5.c
@@ -12,47 +12,46 @@
 
 // ne menuvaj ovde
 void wf() {
-  FILE *f = fopen("livce.txt", "w");
-  char c;
-  while((c = getchar()) != '#') {
-      fputc(c, f);
-  }
-  fclose(f);
+    FILE *f = fopen("livce.txt", "w");
+    char c;
+    while((c = getchar()) != '#') { fputc(c, f); }
+    fclose(f);
 }
 
 int main() {
-  wf();
-  // vasiot kod pocnuva od ovde
+    wf();
+    // vasiot kod pocnuva od ovde
 
-  FILE *f;
-  if(!(f = fopen("livce.txt", "r"))) return -1;
+    FILE *f;
+    if(!(f = fopen("livce.txt", "r"))) return -1;
 
-  int n, indeks = 0;
-  char shifra[100][100];
-  int tip[100];
-  float koef[100];
+    int n, indeks = 0;
+    char shifra[100][100];
+    int tip[100];
+    float koef[100];
 
-  fscanf(f, "%d", &n);
+    fscanf(f, "%d", &n);
 
-  while(!feof(f)) {
-    fscanf(f, "%s %d %f", shifra[indeks], &tip[indeks], &koef[indeks]);
-    if(feof(f)) break;
-    indeks++;
-  }
-
-  float s = n, maxKoeficient = 0;
-  int r = indeks;
-
-  for(int i = 0; i < indeks; i++) {
-    s *= koef[i];
-
-    if(koef[i] > maxKoeficient) {
-      maxKoeficient = koef[i];
-      r = i;
+    while(!feof(f)) {
+        fscanf(f, "%s %d %f", shifra[indeks], &tip[indeks], &koef[indeks]);
+        if(feof(f)) break;
+        indeks++;
     }
-  }
 
-  printf("%s %d %.2f\n%.2f", shifra[r], tip[r], koef[r], s);
+    float s = n, maxKoeficient = 0;
+    int r = indeks;
 
-  return 0;
+    for(int i = 0; i < indeks; i++) {
+        s *= koef[i];
+
+        if(koef[i] > maxKoeficient) {
+            maxKoeficient = koef[i];
+            r = i;
+        }
+    }
+
+    printf("%s %d %.2f\n%.2f", shifra[r], tip[r], koef[r], s);
+
+    return 0;
 }
+
