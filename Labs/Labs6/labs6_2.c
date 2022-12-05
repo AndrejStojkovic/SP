4c5b86242fa79c530541a1a786e983081d6f3494
Andrej Stojkovikj <40241148+AndrejStojkovic@users.noreply.github.com>
2022-12-05 08:40:22 +0100

    Update labs6_2.c

--- Labs/Labs6/labs6_2.c
+++ Labs/Labs6/labs6_2.c
@@ -19,16 +19,13 @@ int main() {
     if(n > 100) { return 0; }
 
     for(int i = 0; i < n; i++) {
-        int niza[100];
+        int niza[100], pomosh = 0;
 
         for(int j = 0; j < n; j++) { scanf("%d", &niza[j]); }
-
-        int pomosh = 0;
 
         for(int k = 0; k < n - 1; k++) {
             if(niza[k] < niza[k + 1]) {
                 pomosh++;
-
                 if(pomosh > indeks) indeks = pomosh;
             }
         }
