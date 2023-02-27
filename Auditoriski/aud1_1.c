d05c953ebffce82bc155c43b8b8f63172cdef03f
Andrej Stojkovikj <40241148+AndrejStojkovic@users.noreply.github.com>
2023-02-28 00:41:17 +0100

    Delete Auditoriski directory

--- Auditoriski/aud1_1.c
+++
@@ -1,8 +0,0 @@
-// 1.1
-
-#include <stdio.h>
-
-int main() {
-  printf("Zdravo, kako si?\n");
-  return 0;
-}
--- Auditoriski/aud2_2.c
+++
@@ -1,9 +0,0 @@
-// 2.2
-
-#include <stdio.h>
-
-int main() {
-  int x = 5;
-  printf("Brojot %d na kvadrat e %d.", x, x * x);
-  return 0;
-}
